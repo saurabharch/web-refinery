d5b74b90c10a80a781a57c438a2912679f514719
Yuriy Ivanyuk <ivanyyuk@gmail.com>
2016-08-15 12:16:34 -0400

    design

---
+++ browser/js/common/directives/element-menu/element-menu.js
@@ -0,0 +1,7 @@
+app.directive('elementMenu', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/element-menu/element-menu.html',
+        controller: 'TabsDemoCtrl'
+    }
+})
