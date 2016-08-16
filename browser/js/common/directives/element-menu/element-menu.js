3f89d6aa9bc408457a41093f807c5b1ddb584070
Richard Chao <r.chao03@gmail.com>
2016-08-16 10:56:06 -0400

    add image is in collapsable

--- browser/js/common/directives/element-menu/element-menu.js
+++ browser/js/common/directives/element-menu/element-menu.js
@@ -1,5 +1,6 @@
 app.directive('elementMenu', function () {
     return {
+        transclude:true,
         restrict: 'E',
         templateUrl: 'js/common/directives/element-menu/element-menu.html',
     }
