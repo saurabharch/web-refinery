0fd50d12d31d23277deecb234ed4bd7596739e1c
jcduran05 <jcduran05@gmail.com>
2016-08-17 16:56:45 -0400

    Remove buttons from textAngular.

--- browser/js/app.js
+++ browser/js/app.js
@@ -10,7 +10,36 @@ app.config(function ($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.when('/auth/:provider', function () {
         window.location.reload();
     });
+
+
 });
+
+app.config(['$provide', function($provide){
+    // this demonstrates how to register a new tool and add it to the default toolbar
+    $provide.decorator('taOptions', ['$delegate', function(taOptions){
+        // $delegate is the taOptions we are decorating
+        // here we override the default toolbars and classes specified in taOptions.
+        taOptions.forceTextAngularSanitize = true; // set false to allow the textAngular-sanitize provider to be replaced
+        taOptions.keyMappings = []; // allow customizable keyMappings for specialized key boards or languages
+        taOptions.toolbar = [
+            ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+            ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+            // ['justifyLeft','justifyCenter','justifyRight', 'justifyFull'],
+            ['html', 'insertLink']
+        ];
+        taOptions.classes = {
+            focussed: 'focussed',
+            toolbar: 'btn-toolbar',
+            toolbarGroup: 'btn-group',
+            toolbarButton: 'btn btn-default',
+            toolbarButtonActive: 'active',
+            disabled: 'disabled',
+            textEditor: 'form-control',
+            htmlEditor: 'form-control'
+        };
+        return taOptions; // whatever you return will be the taOptions
+    }]);
+}]);
 
 // This app.run is for controlling access to specific states.
 app.run(function ($rootScope, AuthService, $state) {
