a6a2324aea0a41bd471195052d45f6c7c3dce90e
jcduran05 <jcduran05@gmail.com>
2016-08-19 16:27:41 -0400

    Update to ths style for the editor page.

---
+++ browser/js/common/directives/navbar-editor/navbar-editor.js
@@ -0,0 +1,47 @@
+app.directive('navbarEditor', function ($rootScope, AuthService, AUTH_EVENTS, $state) {
+
+    return {
+        restrict: 'EA',
+        scope: {},
+        templateUrl: 'js/common/directives/navbar-editor/navbar-editor.html',
+        link: function (scope) {
+            scope.items = [
+                { label: 'Home', state: 'home' },
+                // { label: 'Editor', state: 'editor' },
+                { label: 'Dashboard', state: 'dashboard', auth:true },
+                // { label: 'Members Only', state: 'membersOnly', auth: true }
+            ];
+
+            scope.user = null;
+
+            scope.isLoggedIn = function () {
+                return AuthService.isAuthenticated();
+            };
+
+            scope.logout = function () {
+                AuthService.logout().then(function () {
+                   $state.go('home');
+                });
+            };
+
+            var setUser = function () {
+                AuthService.getLoggedInUser().then(function (user) {
+                    scope.user = user;
+                });
+            };
+
+            var removeUser = function () {
+                scope.user = null;
+            };
+
+            setUser();
+
+            $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
+            $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
+            $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
+
+        }
+
+    };
+
+});
