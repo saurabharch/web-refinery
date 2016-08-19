67728f18c5fb2e574fc94b60f95a003534d56c23
Richard Chao <r.chao03@gmail.com>
2016-08-19 13:05:02 -0400

    css friday

--- browser/js/common/directives/element-menu/element-menu.js
+++ browser/js/common/directives/element-menu/element-menu.js
@@ -3,5 +3,17 @@ app.directive('elementMenu', function () {
         transclude:true,
         restrict: 'E',
         templateUrl: 'js/common/directives/element-menu/element-menu.html',
-    }
-})
+        link: function(scope, element, attr){
+          scope.click = function($event){
+            var currentElement = $($event.currentTarget)
+            if(currentElement.hasClass("activeCategory")){
+              currentElement.removeClass("activeCategory")
+            } else {
+            $(".mySideNav").find(".category-links").removeClass("activeCategory").collapse()
+           currentElement.addClass("activeCategory")
+           }
+           }
+          }
+          }
+        }
+    )
