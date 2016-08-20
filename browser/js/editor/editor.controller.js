9683fb0ad097a7617d25b4a43fabebf745aeae4f
Chang Ethan Lee <celcoder@gmail.com>
2016-08-20 11:54:33 -0400

    testing animation

--- browser/js/editor/editor.controller.js
+++ browser/js/editor/editor.controller.js
@@ -1,13 +1,12 @@
-
-app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, textSelected) {
+app.controller('ModalInstanceCtrl', function($scope, $uibModalInstance, textSelected) {
 
   $scope.textSelected = textSelected;
 
-  $scope.ok = function () {
+  $scope.ok = function() {
     $uibModalInstance.close($scope.textSelected);
   };
 
-  $scope.cancel = function () {
+  $scope.cancel = function() {
     $uibModalInstance.dismiss('cancel');
   };
 });
@@ -17,7 +16,7 @@ app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, textSel
 app.controller('EditorCtrl', function($scope, fileUpload, ProjectFactory, PageFactory, currentProject, ImageFactory, allImages, $uibModal, $log) {
 
   $scope.animationsEnabled = false;
-  $scope.open = function (size) {
+  $scope.open = function(size) {
 
     var modalInstance = $uibModal.open({
       animation: false,
@@ -25,28 +24,28 @@ app.controller('EditorCtrl', function($scope, fileUpload, ProjectFactory, PageFa
       controller: 'ModalInstanceCtrl',
       size: size,
       resolve: {
-        textSelected: function () {
+        textSelected: function() {
           return $scope.textSelected;
         }
       }
     });
 
-    modalInstance.result.then(function (editedModalText) {
+    modalInstance.result.then(function(editedModalText) {
       $scope.textSelected = editedModalText;
 
       $($scope.textTag).html($scope.textSelected);
 
-    }, function () {
+    }, function() {
       $log.info('Modal dismissed at: ' + new Date());
     });
   };
 
-  $scope.toggleAnimation = function () {
+  $scope.toggleAnimation = function() {
     $scope.animationsEnabled = !$scope.animationsEnabled;
   };
 
   //makes all elements in body editable
-  $scope.edit = function () {
+  $scope.edit = function() {
     $('#skeleton').contents().find('h1,h2,h3,h4,h5,h6,p,span,button,a').each(function() {
       var self = $(this);
       var handlerIn = function() {
@@ -54,41 +53,41 @@ app.controller('EditorCtrl', function($scope, fileUpload, ProjectFactory, PageFa
       };
 
       var handlerOut = function() {
-        if (!self.hasClass('changeThis')){
+        if (!self.hasClass('changeThis')) {
           self.css('border', '');
-  
-      }
-    };
+
+        }
+      };
 
       // Function to give the element that you
       // are hovering over some style and remove it
       // Only works with find('h1,h2,h3,h4,h5,h6,p,span,button,a')
       if (self.text() != '') self.hover(handlerIn, handlerOut);
-      
-      if(!self.hasClass('alreadyEditable') ) {
+
+      if (!self.hasClass('alreadyEditable')) {
         self.addClass('alreadyEditable')
         self.dblclick(function() {
 
-        $scope.textSelected = $(this)[0].outerHTML;
-        $scope.textTag = $(this);
-        $scope.open();
-      })
+          $scope.textSelected = $(this)[0].outerHTML;
+          $scope.textTag = $(this);
+          $scope.open();
+        })
 
       }
-      
+
 
 
     });
-      }
+  }
 
-      $scope.createProject = function (obj){
-        ProjectFactory.create(obj)
-      }
+  $scope.createProject = function(obj) {
+    ProjectFactory.create(obj)
+  }
 
-  
+
   //will make a hardcoded request to backend and use the archiver to zip up the project
   $scope.currentProject = currentProject.id;
-  
+
 
   $scope.projectUrl = 'hosted-projects/' + currentProject.id + '/index.html';
 
@@ -102,14 +101,14 @@ app.controller('EditorCtrl', function($scope, fileUpload, ProjectFactory, PageFa
       file: $scope.file
     }
 
-    if($scope.file.type!=="image/png" && $scope.file.type!=="image/jpeg" && $scope.file.type!=="image/jpg"){
+    if ($scope.file.type !== "image/png" && $scope.file.type !== "image/jpeg" && $scope.file.type !== "image/jpg") {
       alert("Please upload an image")
-    }else {
+    } else {
       return fileUpload.upload(uploadObj, uploadUrl)
-      .then(function(imageArray) {
-        $scope.allImages = imageArray;
-        // console.log($scope.allImages)
-      })
+        .then(function(imageArray) {
+          $scope.allImages = imageArray;
+          // console.log($scope.allImages)
+        })
     }
   }
 
@@ -118,7 +117,7 @@ app.controller('EditorCtrl', function($scope, fileUpload, ProjectFactory, PageFa
     $scope.colorBool = !$scope.colorBool;
   }
 
-  $('#skeleton').on('load',function() {
+  $('#skeleton').on('load', function() {
     $scope.edit();
     // $('#skeleton').find('img').on('click', function(event){
     //   $(event.target).attr('draggable', 'true')
